30e8f698d3157839170af8ff6d33dff7a457c981
Omar <ohammad@gwu.edu>
2023-12-06 15:12:01 -0500

    finished tree order project

--- tree_order_project/tree_order_project/lib/tree_order.js
+++ tree_order_project/tree_order_project/lib/tree_order.js
@@ -1,9 +1,22 @@
-function inOrderArray(root) {
+function inOrderArray(root, result=[]) {
+    if (!root) return [];
 
+
+    inOrderArray(root.left, result);
+    result.push(root.val);
+    inOrderArray(root.right, result);
+
+    return result;
 }
 
-function postOrderArray(root) {
+function postOrderArray(root, result=[]) {
+    if (!root) return [];
 
+    postOrderArray(root.left, result);
+    postOrderArray(root.right, result);
+    result.push(root.val);
+
+    return result;
 }
 
 
